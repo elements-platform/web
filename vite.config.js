9ab8b7941f0f4471e7600a4f12b93afa1fe21248
KaMeHb-UA <marlock@bassteam.ml>
2021-02-13 14:09:20 +0200

    add base styles

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,19 @@ import preactRefresh from '@prefresh/vite'
  * @type { import('vite').UserConfig }
  */
 const config = {
-  esbuild: {
-    jsxFactory: 'h',
-    jsxFragment: 'Fragment',
-    jsxInject: `import { h, Fragment } from 'preact'`
-  },
-  plugins: [preactRefresh()]
+	esbuild: {
+		jsxFactory: 'h',
+		jsxFragment: 'Fragment',
+		jsxInject: `import { h, Fragment } from 'preact'`
+	},
+	plugins: [
+		preactRefresh(),
+	],
+	css: {
+		modules: {
+			scopeBehaviour: 'local',
+		},
+	},
 }
 
 export default config

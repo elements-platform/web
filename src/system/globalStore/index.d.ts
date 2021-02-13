b6ae70995e3d48e0b535d3af11cc4dcb3fecb04d
KaMeHb-UA <marlock@bassteam.ml>
2021-02-13 23:32:43 +0200

    added strictly-typed global store

---
+++ src/system/globalStore/index.d.ts
@@ -0,0 +1,16 @@
+import API from '../../system/api'
+
+interface GlobalStore<T extends { [x: string]: any }> extends Map<keyof any, any>{
+    delete<K extends keyof T>(key: K): boolean;
+    forEach<K extends keyof T>(callback: (value: T[K], key: K, map: GlobalStore<T>) => void, thisArg?: any): void;
+    get<K extends keyof T>(key: K): T[K] | undefined;
+    has<K extends keyof T>(key: string): false;
+    has<K extends keyof T>(key: K): true;
+    set<K extends keyof T>(key: K, value: T[K]): this;
+}
+
+declare const globalStore: GlobalStore<{
+    api: API
+}>
+
+export default globalStore

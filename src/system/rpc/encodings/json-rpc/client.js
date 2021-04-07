16978216dfd86aefe94d2d41699da9f9b6d830cd
KaMeHb-UA <marlock@bassteam.ml>
2021-04-07 13:08:44 +0300

    support for JSON-RPC 2.0, broke support for 1.0

--- src/system/rpc/encodings/json-rpc/client.js
+++ src/system/rpc/encodings/json-rpc/client.js
@@ -1,15 +1,16 @@
 import NamedError from '@/system/helpers/namedError'
 
 class JSONRPCError extends NamedError{
+	/** @arg {{ code: number, message: string, data?: { stack?: string } }} arg_0 */
 	constructor({ code, message, data }){
 		super(message);
 		this.code = code;
 		if(data?.stack){
 			const stack = (this.stack || '').split('\n');
 			const message = stack.shift();
-			const prefix = '\n' + /^\s*\w+\s*/.exec(stack[0])[0];
-			const remoteStack = ['', ...data.stack].join(prefix);
-			this.stack = [ message, remoteStack, ...stack ].join('\n')
+			const prefix = '\n' + /^\s*\S+\s*/.exec(stack[0])[0];
+			const remoteStack = ['', ...data.stack.split('\n')].join(prefix).slice(1);
+			this.stack = [ message, remoteStack, ...stack ].join('\n');
 		}
 	}
 }
@@ -25,10 +26,21 @@ export default transport => {
 	const encodedTransport = {
 		send,
 		encode(data){
-			return JSON.stringify(data)
+			const params = data.args;
+			const fixed = Object.assign({ params, jsonrpc: '2.0' }, data);
+			delete fixed.args;
+			return JSON.stringify(fixed)
 		},
+		/** @arg {any} data */
 		decode(data){
-			const { id, error, result } = JSON.parse(data);
+			const { jsonrpc, id, error, result } = data;
+			if(jsonrpc !== '2.0') return {
+				id,
+				error: new JSONRPCError({
+					code: -1,
+					message: '"jsonrpc" field of the responce is not equivalent "2.0"'
+				}),
+			};
 			if(error) return { id, error: new JSONRPCError(error) };
 			return { id, result }
 		},
@@ -45,6 +57,7 @@ export default transport => {
 
 	function sendStack(){
 		const data = currentCallStack.length === 1 ? currentCallStack[0] : `[${currentCallStack.join(',')}]`;
+		currentCallStack = [];
 		transport.send(data)
 	}
 

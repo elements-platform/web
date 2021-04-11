916e43d5cb1e0475041e4c77c1a2f51fa4b2420c
KaMeHb-UA <marlock@bassteam.ml>
2021-04-11 13:11:21 +0300

    page base

---
+++ src/elements/page/index.jsx
@@ -0,0 +1,24 @@
+import globalStore from '@/system/global-store'
+import { Component } from 'preact'
+import render from '@/system/render'
+
+export default class Page extends Component{
+	props = {
+		title: '',
+		icon: '',
+		children: [],
+	}
+	render(){
+		const { children } = this.props;
+		return children.map(v => render(v))
+	}
+}
+
+function renderPage(params){
+	globalStore.get('setPage')(<Page {...params}/>)
+	return null
+}
+
+export {
+	renderPage as render
+}

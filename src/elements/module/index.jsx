c08c19b22c07f8f0444b57bf0d95005d6d29e29b
KaMeHb-UA <marlock@bassteam.ml>
2021-06-28 23:22:26 +0300

    fix module element

--- src/elements/module/index.jsx
+++ src/elements/module/index.jsx
@@ -2,9 +2,16 @@ import { Component } from 'preact';
 import exec from '@/core/builder';
 
 export default class Module extends Component{
+	props = {
+		url: '',
+	}
 	render(){
 		const { url } = this.props;
 		exec(url);
 		return null;
 	}
+}
+
+export function render(params){
+	return <Module {...params}/>
 }
